bec29140a1fda3573307fb8097d930a78a333168
kuzen <kuzen@users.noreply.github.com>
2022-01-24 13:59:38 +0800

    优化按钮逻辑

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  rules: {
+    'max-len': 0,
+    'require-jsdoc': 0,
+    'camelcase': 0,
+    'eqeqeq': [1, 'allow-null'],
+    'handle-callback-err': [1, '^(err|error)$'],
+    'jsx-quotes': [1, 'prefer-single'],
+    'new-cap': [1, {newIsCap: true, capIsNew: false}],
+    'no-extra-parens': [1, 'functions'],
+    'no-inner-declarations': [1, 'functions'],
+    'no-labels': [1, {allowLoop: false, allowSwitch: false}],
+    'no-multi-spaces': [1, {ignoreEOLComments: true}],
+    'no-return-assign': [1, 'except-parens'],
+    'no-unneeded-ternary': [1, {defaultAssignment: false}],
+    'no-unused-vars': [1, {vars: 'all', args: 'none'}],
+    'one-var': [1, {initialized: 'never'}],
+    'no-console': 0,
+    'no-undef': 0,
+  },
+};
